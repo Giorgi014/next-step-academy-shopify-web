4f09b35f1e3d7b0df5345cad9a368ebe2f2d4d57
Giorgi014 <giorgi.gugunava99@gmail.com>
2024-09-01 00:29:52 +0400

    link updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { IoSearchOutline } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { LuMoon } from "react-icons/lu";
+import {NavLink,Link} from "react-router-dom"
 import './Navbar.css'
 
 
@@ -9,7 +10,7 @@ function Navbar() {
     return ( 
         <nav className="nav-bar">
             <div className="logo">
-                <h1>SHOPIFY</h1>
+                <h1><Link to={"/"}>SHOPIFY</Link></h1>
             </div>
             <div className="search-place">
                 <div className="shearch-bar">
@@ -18,8 +19,8 @@ function Navbar() {
                 </div>
             </div>
             <ul className="nav-right-side">
-                <li>Products</li>
-                <li>Categories</li>
+                <li><NavLink to={"products"}>Products</NavLink></li>
+                <li><NavLink to={"categories"}>Categories</NavLink></li>
                 <li className="icon-and-text"><FaUser className="secondary-color-b" /><span className="secondary-color">Login</span></li>
                 <li className="big-size secondary-color"><AiOutlineShoppingCart /><div className="cart-count">0</div></li>
                 <li className="big-size"><LuMoon /></li>
