d640901fec067718009d6a19db10b9ff17802a73
Giorgi014 <giorgi.gugunava99@gmail.com>
2024-10-09 14:17:48 +0400

    error fix

--- src/components/helper/api.js
+++ src/components/helper/api.js
@@ -3,38 +3,68 @@ import axios from "axios";
 const baseUrl = "https://dummyjson.com/";
 
 export const getAllProducts = async (limit, skip) => {
-  const resp = await axios.get(`${baseUrl}products`, {
-    params: {
-      limit,
-      skip,
-    },
-  });
-
-  return resp.data;
+  try {
+    const resp = await axios.get(`${baseUrl}products`, {
+      params: {
+        limit,
+        skip,
+      },
+    });
+    return resp.data;
+  } catch (error) {
+    return error.code
+  }
+  
 };
 
 export const getSingleProduct = async (id) => {
-  const resp = await axios.get(`${baseUrl}product/${id}`);
-  return resp.data;
+  try {
+    const resp = await axios.get(`${baseUrl}product/${id}`);
+    return resp.data;
+  } catch (error) {
+    return error
+  }
 };
 
 export const getAllCategories = async () => {
-  const resp = await axios.get(`${baseUrl}products/categories`);
-  return resp.data;
+  try {
+    const resp = await axios.get(`${baseUrl}products/categories`);
+    return resp.data;
+  } catch (error) {
+    return error.code
+  }
+  
 };
 export const getProductsByCategory = async (id) => {
-  const resp = await axios.get(`${baseUrl}products/category/${id}`);
-  return resp.data.products;
+  try {
+    const resp = await axios.get(`${baseUrl}products/category/${id}`);
+    return resp.data.products;
+  } catch (error) {
+    return error
+  }
+  
 };
 export const getToken = async (userInfo) => {
-  const resp = await axios.post(`${baseUrl}auth/login`, userInfo);
-  return resp.data.accessToken;
+  try {
+    const resp = await axios.post(`${baseUrl}auth/login`, userInfo);
+    return resp.data.accessToken;
+  } catch (error) {
+    return error
+  }
 };
 export const getDataUsingToken = async (token) => {
-  const resp = await axios.get(`${baseUrl}user/me`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return resp.data;
+  try{
+    const resp = await axios.get(`${baseUrl}user/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if(!resp.status===200 && !resp.status===201){
+      throw new Error("Could not get data")
+    }else{
+      return resp.data;
+    }
+  }catch(error){
+    return false
+  }
 };
